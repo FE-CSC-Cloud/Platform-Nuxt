53fbc4d9035ad791c1e5aecc26ea98e712a022f9
Cornell van der Straaten <cornellbvanders@gmail.com>
2023-04-18 17:22:24 +0200

    Button to request a server page

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <p class="font-bold">Dashboard</p>
+    <button v-on:click="navigateTo('request')" type="button" class="cursor-pointer rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-dark">Server aanvragen</button>
 </template>
 
 <script>
