2cab4e8a147d4615b8bc7444ee0a935bd8aaf5bd
Cornell van der Straaten <cornellbvanders@gmail.com>
2023-04-18 18:26:40 +0200

    Added HU colors and link to HU website

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,17 @@
 <template>
     <footer class="bg-gray-300">
         <div class="py-3">
-            <p class="text-center text-xs leading-5 text-gray-500">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+            <p class="text-center text-xs leading-5 text-gray-500">&copy; 2023 <span v-if="currentYear != 2023"> - {{ currentYear }} </span> <a href="https://www.hu.nl/" target="_blank" class="text-primary">Hogeschool <span class="text-secondary">Utrecht</span></a> - Frontend CSC Cloud. All rights reserved.</p>
         </div>
     </footer>
-  </template>
+</template>
+
+<script>
+export default {
+    setup() {
+        let currentYear = new Date().getFullYear()
+
+        return {currentYear}
+    }
+}
+</script>
