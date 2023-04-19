e2a577e739bf64393972a55cff35d5bfe9e549ba
Cornell van der Straaten <cornellbvanders@gmail.com>
2023-04-19 10:48:10 +0200

    Added validation errors and messages

---
+++ composables/classes.js
@@ -0,0 +1,3 @@
+export const getInputClasses = () => {
+    return Object.freeze({ Default: 'text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-primary', Errors: 'text-red-900 focus-visible:outline-none ring-red-400 placeholder:text-red-400 focus:ring-red-500'})
+}
--- pages/request.vue
+++ pages/request.vue
@@ -1,28 +1,37 @@
 <template>
     <h1 class="font-bold text-2xl">Aanvraag maken</h1>
-    <p class="mt-2 text-gray-800 w-1/2 text-sm">Stel hier in welke instellingen je wilt voor op je server. Als je over een limiet gaat wordt bij het insturen
+    <p class="mt-2 text-gray-800 md:w-3/4 w-full text-md">Stel hier in welke instellingen je wilt voor op je server. Als je over een limiet gaat wordt bij het insturen
       van de aanvraag de server wel aangemaakt, maar wordt er bij de beheerders een aanvraag ingediend. De beheerders bekijken dan of zij de wijzigingen toestaan.
     </p>
     <div class="mt-5">
-      <div class="w-1/3 flex flex-col gap-3">
+      <div class="md:w-1/3 w-full flex flex-col gap-3">
         <div>
           <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Project naam<span class="text-red-500">*</span></label>
           <div class="mt-1">
-            <input type="text" name="name" id="name" v-model="request.name" class="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
+            <input type="text" name="name" id="name" v-model="state.name" @blur="v$.name.$touch" :class="[v$.name.$errors.length ? inputClasses.Errors : inputClasses.Default]" class="block w-full border-0 py-1.5 rounded-md pl-4 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 " />
+          </div>
+          <div class="text-xs text-red-500 mt-1" v-for="error of v$.name.$errors" :key="error.$uid">
+            <div class="error-msg">{{ error.$message }}</div>
           </div>
         </div>
         <div>
           <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Project beschrijving<span class="text-red-500">*</span></label>
           <div class="mt-1">
-            <textarea rows="3" name="description" id="description" v-model="request.description" class="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
+            <textarea rows="3" name="description" id="description" v-model="state.description" @blur="v$.description.$touch" :class="[v$.description.$errors.length ? inputClasses.Errors : inputClasses.Default]" class="block w-full border-0 py-1.5 rounded-md pl-4 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 " />
+          </div>
+          <div class="text-xs text-red-500 mt-1" v-for="error of v$.description.$errors" :key="error.$uid">
+            <div class="error-msg">{{ error.$message }}</div>
           </div>
         </div>
         <div>
           <label for="os" class="block text-sm font-medium leading-6 text-gray-900">Besturingssysteem<span class="text-red-500">*</span></label>
-          <select id="os" name="os" v-model="request.os" class="mt-1 block w-full rounded-md border-0 py-2 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6">
+          <select id="os" name="os" v-model="state.os" @blur="v$.os.$touch" :class="[v$.os.$errors.length ? inputClasses.Errors : inputClasses.Default]" class="block w-full border-0 py-1.5 rounded-md pl-4 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 ">
             <option selected disabled value="null">Maak een selectie</option>
             <option v-for="option in os_options" :key="option.id" :value="option.id">{{ option.name }}</option>
           </select>
+          <div class="text-xs text-red-500 mt-1" v-for="error of v$.os.$errors" :key="error.$uid">
+            <div class="error-msg">{{ error.$message }}</div>
+          </div>
         </div>
       </div>
       <div class="mt-5">
@@ -36,23 +45,41 @@ definePageMeta({
   layout: "dashboard"
 })
 
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
+
 export default {
-  data() {
-    return {
-      request: {
+  setup() {
+    const state = reactive({
         name: '',
         description: '',
         os: null
-      },
+      })
+
+    const rules = {
+      name: {required},
+      description: {required},
+      os: {required},
+    }
+
+    const v$ = useVuelidate(rules, state)
+
+    return { state, v$ }
+  },
+  data() {
+    return {
       os_options: [
         {id: 1, name: 'Ubuntu'},
         {id: 2, name: 'Windows'}
-      ]
+      ],
+      inputClasses: getInputClasses()
     }
   },
   methods: {
-    submit() {
-      alert(this.request.name)
+    async submit() {
+      let formCorrect = await this.v$.$validate()
+      if (formCorrect) alert(this.state.name, formCorrect)
     }
   }
 }
