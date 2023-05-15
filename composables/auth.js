5d7312b7b8a3abd932c7ec5b905a91d63f3ef0f7
Cornell van der Straaten <cornellbvanders@gmail.com>
2023-05-15 09:23:57 +0200

    Added code comments

--- composables/auth.js
+++ composables/auth.js
@@ -1,21 +1,22 @@
 import sha256 from 'crypto-js/sha256'
 
 export const setAuthToken = async (token) => {
-    // Set cookie
     let expiryDate = new Date().getTime() + 43200000 // 12 hours
+    // Get cookie, set value and expiry date
     const accessToken = useCookie('access_token')
     accessToken.value = token
     accessToken.expires = expiryDate
 }
 
 export const setAuthUser = async (user) => {
-    // Set cookie
     let expiryDate = new Date().getTime() + 43200000 // 12 hours
+    // Get cookie, set value and expiry date
     const authorizedUser = useCookie('authorized_user')
     authorizedUser.value = user
     authorizedUser.expires = expiryDate
 }
 
+// Remove values from existing cookies which makes auth checks fail
 export const logout = () => {
     const accessToken = useCookie('access_token')
     accessToken.value = null
