744fc6dd1a60a148688522887a8f730b4c9196c5
Cornell van der Straaten <cornellbvanders@gmail.com>
2023-05-22 11:46:24 +0200

    Redirect from index to login

--- middleware/redirectUnauthenticatedUser.global.js
+++ middleware/redirectUnauthenticatedUser.global.js
@@ -6,6 +6,12 @@ export default defineNuxtRouteMiddleware((to, from) => {
     ]
 
     const access_token = useCookie('access_token')
+    if (to.name == 'index') {
+        useRouter().push({
+            path: '/login',
+        })
+    }
+
     if (allowedUnauthorizedRouteNames.includes(to.name)) {
         // Redirect from unauthorized routes if logged in
         if (access_token.value != undefined) {
